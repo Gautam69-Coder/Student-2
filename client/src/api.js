86cd1058c6d9a66ae906302d8adf518950666feb
Gautam-College <gautamdoliya69@gmail.com>
2025-12-09 00:39:29 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API_URL = 'https://student-54b8.onrender.com/api';
 // const API_URL = 'http://localhost:5000/api';
-const API_URL = 'https://student-3-production.up.railway.app/api';
+const API_URL = 'https://authentic-trust-production-aa48.up.railway.app';
 
 const api = axios.create({
     baseURL: API_URL,

5b1696b4b0ebc0b380542109ac2e0cd2f8bdf1fe
Gautam-College <gautamdoliya69@gmail.com>
2026-01-24 22:50:54 +0530

    changes

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['user', 'admin', 'superadmin'], default: 'user' },
-    visitCount: { type: Number, default: 0 }
+    visitCount: { type: Number, default: 1 }
 });
 
 module.exports = mongoose.model('User', UserSchema);

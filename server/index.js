1b2bdf5213a2231ba82ea52e9ac89b632ea25e90
Gautam-College <gautamdoliya69@gmail.com>
2026-02-18 17:29:40 +0530

    fix socket url and email config

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://student-2-3ow8.onrender.com', 'https://student-2.pages.dev'],
+        origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://student-2-3ow8.onrender.com', 'https://student-2.pages.dev', 'https://student-2-temprory.onrender.com'],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
         credentials: true
     }
--- server/routes/email.js
+++ server/routes/email.js
@@ -7,8 +7,8 @@ const User = require('../models/User');
 // Configure Nodemailer Transporter
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // Use SSL
+    port: 587,
+    secure: false, // Use SSL
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS

2a596cea2d5471d23cc648dbf71c7df16ec0d263
gautam-gollege <gautamdoliya69@gmail.com>
2026-05-14 14:33:01 +0530

    try to fix

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -2,7 +2,7 @@ import multer from "multer"
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "../server/uploads")  // ✅ yeh folder banao project mein
+        cb(null, "../uploads")  // ✅ yeh folder banao project mein
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + "-" + file.originalname)
